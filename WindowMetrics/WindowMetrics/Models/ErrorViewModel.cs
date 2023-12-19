737a12987a998c4e0a9e2308f5a3a90c0638f441
shani patel <shanibpatel@outlook.com>
2023-12-20 02:27:53 +0530

    Intial Commit

---
+++ WindowMetrics/WindowMetrics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WindowMetrics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
